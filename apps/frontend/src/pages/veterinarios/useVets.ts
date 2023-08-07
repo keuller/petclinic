1d8a54128422c58e2a8f21c6ae4be6d9f9c2ad0a
Keuller Magalhaes <keuller.magalhaes@gmail.com>
2023-08-07 15:37:38 +0100

    typo: fixtures typo

--- apps/frontend/src/pages/veterinarios/useVets.ts
+++ apps/frontend/src/pages/veterinarios/useVets.ts
@@ -1,10 +1,10 @@
 
 const FIXTURES = [
-    { id: '4878', nome: 'Veterianrio Test 1', especialidades: ['cirurgia'] },
-    { id: '3452', nome: 'Veterianrio Test 2', especialidades: ['radiologia'] },
-    { id: '1847', nome: 'Veterianrio Test 3', especialidades: ['dentista'] },
-    { id: '8492', nome: 'Veterianrio Test 4', especialidades: [''] },
-    { id: '9823', nome: 'Veterianrio Test 5', especialidades: ['cirurgia', 'obstetricia'] }
+    { id: '4878', nome: 'Veterinario Test 1', especialidades: ['cirurgia'] },
+    { id: '3452', nome: 'Veterinario Test 2', especialidades: ['radiologia'] },
+    { id: '1847', nome: 'Veterinario Test 3', especialidades: ['dentista'] },
+    { id: '8492', nome: 'Veterinario Test 4', especialidades: [''] },
+    { id: '9823', nome: 'Veterinario Test 5', especialidades: ['cirurgia', 'obstetricia'] }
 ]
 
 export default function useVets() {
